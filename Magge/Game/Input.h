270fda6dfb17127e5eb6f779e00939b1859ae550
augustlewen <augustlewen@live.com>
2024-02-29 12:00:58 +0100

    Aded input class to define inputs with enums

---
+++ Magge/Game/Input.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <SDL.h>
+
+
+//Key press surfaces constants
+enum Key
+{
+	Up,
+	Down,
+	Left,
+	Right,
+	Space,
+	Escape,
+
+	Null
+
+};
+
+class Input
+{
+public:
+	Key KeyPressed(SDL_Keycode input) const
+	{
+		switch (input)
+		{
+			case SDLK_LEFT: return Left;
+			case SDLK_RIGHT: return Right;
+			case SDLK_UP: return Up;
+			case SDLK_DOWN: return Down;
+			case SDLK_SPACE: return Space;
+			case SDLK_ESCAPE: return Escape;
+
+			default: return Null;
+		}
+	}
+};
+
--- Magge/Game/Main.cpp
+++ Magge/Game/Main.cpp
@@ -5,16 +5,43 @@ and may not be redistributed without written permission.*/
 #include <SDL.h>
 #include <stdio.h>
 #include "Window.h"
+#include "Input.h"
 
+//Event handler
+SDL_Event e;
 
 int main( int argc, char* args[] )
 {
 	Window window{};
+	Input input{ };
 
     SDL_Surface* imageSurface = SDL_LoadBMP("Resources/cat.bmp");
- 
-	window.RenderImage(imageSurface);
 
+	bool quit = false;
+
+	while (!quit)
+	{
+		
+
+		//Handle events on queue
+		while (SDL_PollEvent(&e) != 0)
+		{
+			Key key = input.KeyPressed(e.key.keysym.sym);
+
+			if (key == Key::Up)
+			{
+				window.RenderImage(imageSurface);
+			}
+			else if (key == Key::Down)
+			{
+			}
+			//User requests quit
+			if (e.type == SDL_QUIT || key == Key::Escape)
+			{
+				quit = true;
+			}
+		}
+	}
 
 	window.Close();
 
