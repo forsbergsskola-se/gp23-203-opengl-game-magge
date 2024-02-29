49052526991d8542152a42afe6e4536652a738c7
augustlewen <augustlewen@live.com>
2024-02-29 09:25:09 +0100

    added main.cpp

---
+++ Magge/Game/Main.cpp
@@ -0,0 +1,14 @@
+/*This source code copyrighted by Lazy Foo' Productions 2004-2024
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
+#include "Window.h"
+
+int main( int argc, char* args[] )
+{
+	Window window{};
+
+	return 0;
+}
