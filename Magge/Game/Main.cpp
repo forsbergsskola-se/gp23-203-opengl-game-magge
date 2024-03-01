ec1f6fe1d2ad6ef995d59fdc56c9e9751f56472d
augustlewen <augustlewen@live.com>
2024-03-01 10:16:05 +0100

    added time and capped frame rate

--- Magge/Game/Main.cpp
+++ Magge/Game/Main.cpp
@@ -8,13 +8,21 @@ and may not be redistributed without written permission.*/
 #include "Texture.h"
 #include "Player.h"
 #include "Input.h"
+#include "Time.h"
 
 //Event handler
 SDL_Event e;
 
+const int SCREEN_FPS = 60;
+const int SCREEN_TICK_PER_FRAME = 1000 / SCREEN_FPS;
+
 int main( int argc, char* args[] )
 {
 	Window window{};
+	Time time{};
+	Time capTimer{};
+	time.Start();
+	int countedFrames = 0;
 
 
 	Player player{};
@@ -33,7 +41,11 @@ int main( int argc, char* args[] )
 	while (!quit)
 	{
 		window.Clear();
+		capTimer.Start();
+
 		player.mesh->Render(window.renderer);
+
+
 		//Handle events on queue
 		while (SDL_PollEvent(&e) != 0)
 		{
@@ -41,21 +53,31 @@ int main( int argc, char* args[] )
 
 			player.PlayerInput(e, key);
 
-			
 			//User requests quit
 			if (e.type == SDL_QUIT || key == Key::Escape)
-			{
 				quit = true;
-			}
-
-			
-
-			//Update screen
-			SDL_RenderPresent(window.renderer);
 		}
 
-		player.Update();
+		//Update screen
+		SDL_RenderPresent(window.renderer);
+
+		//Calculate and correct fps
+		float avgFPS = countedFrames / (time.GetTicks() / 1000.f);
+		if (avgFPS > 2000000)
+			avgFPS = 0;
 		
+		player.Update();
+
+		++countedFrames;
+
+		//If frame finished early
+		int frameTicks = capTimer.GetTicks();
+		if (frameTicks < SCREEN_TICK_PER_FRAME)
+		{
+			//Wait remaining time
+			SDL_Delay(SCREEN_TICK_PER_FRAME - frameTicks);
+		}
+
 	}
 
 	window.Close();
---
+++ Magge/Game/Time.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <SDL.h>
+class Time
+{
+public:
+	//Initializes variables
+	Time();
+
+	//The various clock actions
+	void Start();
+	void Stop();
+	void Pause();
+	void Unpause();
+
+	//Gets the timer's time
+	Uint32 GetTicks();
+
+	//Checks the status of the timer
+	bool IsStarted();
+	bool IsPaused();
+
+private:
+	//The clock time when the timer started
+	Uint32 mStartTicks;
+
+	//The ticks stored when the timer was paused
+	Uint32 mPausedTicks;
+
+	//The timer status
+	bool mPaused;
+	bool mStarted;
+};
+
